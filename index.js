9454f57a2b34846c50ccd5745809ba1638310acb
Mohammad Rahman <me@shadikur.com>
2023-07-30 20:17:59 +0200

    Message Controller Added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const userRoute = require("./Routes/userRoute");
 const chatRoute = require("./Routes/chatRoute");
+const messageRoute = require("./Routes/messageRoute");
 
 const port = process.env.PORT || 3000;
 const db_uri = process.env.DB_URI;
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute)
 app.use("/api/chat", chatRoute)
+app.use("/api/messages", messageRoute)
 
 
 mongoose.connect(db_uri, {
